1c82bf4e7459905c44016a059052f18ddef24783
MarcGroef <marcgroefsema@gmail.com>
2015-04-28 10:56:11 +0200

    ititial commit

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/student/ros-vision/catkin_ws/build/test_results"
+export CMAKE_PREFIX_PATH="/home/student/ros-vision/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/student/ros-vision/catkin_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/student/ros-vision/catkin_ws/devel/lib:/home/student/sudo/ros/catkin_ws/devel/lib:/opt/ros/hydro/lib:/home/student/choregraphe-suite-1.14.5-linux64/lib:/home/student/sudo/brain/src/util"
+export PATH="/home/student/ros-vision/catkin_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/student/ros-vision/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/student/ros-vision/catkin_ws/build"
+export PYTHONPATH="/home/student/ros-vision/catkin_ws/devel/lib/python2.7/dist-packages:/home/student/sudo/ros/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/hydro/lib/python2.7/dist-packages:/home/student/choregraphe-suite-1.14.5-linux64/lib:/home/student/sudo/brain/src"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/student/ros-vision/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/student/ros-vision/catkin_ws/src:/home/student/sudo/ros/catkin_ws/src:/opt/ros/hydro/share:/opt/ros/hydro/stacks"
+export ROS_TEST_RESULTS_DIR="/home/student/ros-vision/catkin_ws/build/test_results"

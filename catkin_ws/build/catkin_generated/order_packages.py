dbc6ed59fb92dfcd42f61aac5b1f5168fddb4bd2
MarcGroef <marcgroefsema@gmail.com>
2015-04-28 11:07:54 +0200

    whoops foutje

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/student/ros-vision/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/student/ros-vision/catkin_ws/devel;/home/student/sudo/ros/catkin_ws/devel;/opt/ros/hydro".split(';') if "/home/student/ros-vision/catkin_ws/devel;/home/student/sudo/ros/catkin_ws/devel;/opt/ros/hydro" != "" else []

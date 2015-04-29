5f39c829241043908a318e0501e33d526671c975
Student <student@lab-pc.(none)>
2015-04-29 16:06:14 +0200

    donderdag: extern opencv linking poging

--- catkin_ws/src/openCV_tutorial/src/image_converter.cpp
+++ catkin_ws/src/openCV_tutorial/src/image_converter.cpp
@@ -2,30 +2,30 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+
+#include <opencv/cv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv/cv.h>
 #include <opencv2/nonfree/nonfree.hpp>
 #include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/line_descriptor/descriptor.hpp>
 #include <iostream>
+
+
 
 using namespace std;
 using namespace cv;
 
-void printVector(std::vector<cv::KeyPoint> kp){
- int i,size;
- size = kp.size();
- for(i=0;i<size;i++){
-  std::cout << kp[i].pt << "\n";   
- }
- std::cout << "there were " << size << " features\n";
+
+void printSIFTVector(std::vector<cv::KeyPoint> kp){
+     int i,size;
+     size = kp.size();
+     for(i=0;i<size;i++){
+        std::cout << kp[i].pt << "\n";   
+     }
+     std::cout << "there were " << size << " features\n";
 }
 
-int main(int argc, char* argv[])
-{
-    ros::init(argc,argv,"image_converter");
-    cv::initModule_nonfree();
-    
-    const cv::Mat input = cv::imread(argv[1], 1); //Load as grayscale
+void performSIFT(cv::Mat input){
     cv::SiftFeatureDetector detector;
     std::vector<cv::KeyPoint> keypoints;
     detector.detect(input, keypoints);
@@ -34,8 +34,26 @@ int main(int argc, char* argv[])
     cv::Mat output;
     cv::drawKeypoints(input, keypoints, output);
     cv::imwrite("sift_result.jpg", output);
+}
 
+/*void performLSD(cv::Mat input){
     
+    std::vector<cv::line_descriptor::KeyLine> keylines;
+    cv::Mat output;
+    cv::LSDDetector detector;
+    detector.detect(input,keylines,1,2);
+   
+}*/
+
+
+int main(int argc, char* argv[]){
+    cv::Mat output;
+    
+    ros::init(argc,argv,"image_converter");
+    cv::initModule_nonfree();
+    const cv::Mat input = cv::imread(argv[1], 1); //Load as grayscale
+    performSIFT(input);  
+    //performLSD(input);
     
     return 0;
 }

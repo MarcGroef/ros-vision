5e472c764ea2dc9016cff02a6ca98e1a82e8455d
Student <student@lab-pc.(none)>
2015-06-10 15:33:10 +0200

    forgotten some things to add..

---
+++ catkin_ws/src/bold/src/bold_ml_preproc.hpp
@@ -0,0 +1,6 @@
+#ifndef BOLD_ML_PREPROC
+#define BOLD_ML_PREPROC
+
+  
+
+#endif
---
+++ catkin_ws/src/bold/src/bold_nnet.hpp
@@ -0,0 +1,18 @@
+#ifndef BOLD_NNET_HPP
+#define BOLD_NNET_HPP
+
+
+  using namespace std;
+  using namespace cv;
+  
+  namespace BOLD{
+    
+    class BOLDNetwork{
+	
+      
+    };
+
+  
+  }
+
+#endif
